1ec047bf9d0d8b58fb32cb29a2ae4795f03c7c8c
lmp2000 <lmrpais@gmail.com>
2025-09-29 15:10:52 +0100

    commit 8

--- C07/ft_ultimate_range.c
+++ C07/ft_ultimate_range.c
@@ -2,5 +2,27 @@
 
 int ft_ultimate_range(int **range, int min, int max)
 {
+    int i;
+    int len;
+
+    if (!range)
+        return -1;
     
+    if (min >= max)
+    { 
+        *range = NULL;
+        return 0;
+    }
+
+    len = max - min;
+
+    *range = malloc(len * sizeof(**range));
+    if (!*range)
+        return -1;
+
+    i = 0;
+    while (i < len)
+        (*range)[i] = min + i++;
+    
+    return len;
 }
