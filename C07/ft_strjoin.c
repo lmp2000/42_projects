5a2a54fc014e28811265ba46baace255d55af236
lmp2000 <lmrpais@gmail.com>
2025-09-30 02:14:23 +0100

    commit 11

--- C07/ft_strjoin.c
+++ C07/ft_strjoin.c
@@ -13,30 +13,64 @@ int ft_strlen(char *str)
 int total_len(int size, char **strs, char *sep)
 {
     int i = 0;
-    int total_len = 0;
+    int total = 0;
 
-    while ((*strs)[i] && i < size)
+    if (size <= 0)
+        return 1; // only for the terminating '\0'
+
+    while (i < size && strs[i])
     {
-        total_len += ft_strlen(strs[i]);
+        total += ft_strlen(strs[i]);
         i++;
     }
-    total_len += (ft_strlen(sep) * (size - 1)) + 1;
+    total += (size - 1) * ft_strlen(sep);
+    total += 1; // for '\0'
 
-    return total_len;
+    return total;
 }
 
 char *ft_strjoin(int size, char **strs, char *sep)
 {
     char *strjoin;
     int len;
+    int i = 0;
+    int j;
+    int pos;
 
+    // Handle size == 0: return an allocated empty string that can be freed
     if (size == 0)
     {
-        return NULL;
-        free (strjoin);
+        strjoin = malloc(1);
+        if (!strjoin)
+            return NULL;
+        strjoin[0] = '\0';
+        return strjoin;
     }
 
-    int len = total_len(size, strs, sep);
+    len = total_len(size, strs, sep);
 
     strjoin = malloc(len * sizeof(char));
+    if (!strjoin)
+        return NULL;
+
+    pos = 0;
+    i = 0;
+    while (i < size && strs[i])
+    {
+        j = 0;
+        while (strs[i][j])
+            strjoin[pos++] = strs[i][j++];
+
+        if (i < size - 1)
+        {
+            j = 0;
+            while (sep[j])
+                strjoin[pos++] = sep[j++];
+        }
+        i++;
+    }
+    
+    strjoin[pos] = '\0';
+    
+    return strjoin;
 }
